73449d272f3a1c5514beae508465cbbfd0548ae7
ANTHONY8652 <githinjianthony720@gmail.com>
2025-03-27 12:11:37 +0300

    minor updates

---
+++ blog_posts/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+from .models import BlogPost
+
+class BlogPostFilter(django_filters.FilterSet):
+    published_date = django_filters.DateFilter(field_name="published_date", lookup_expr='exact')
+    category = django_filters.CharFilter(field_name="category", lookup_expr='iexact')
+
+    class Meta:
+        model = BlogPost
+        fields = ['published_date', 'category']
--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,13 +1,24 @@
 from .models import Blog_Post
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from .serializers import Blog_PostSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import BlogPostFilter
 
 
 class Blog_PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Blog_Post.objects.all().order_by('-published_date')
     serializer_class = Blog_PostSerializer
     permission_classes = [IsAdminUser, IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    search_fields = ['title', 'content','published_date', 'author__username', 'tags__name']
+
+    filterset_fields = ['published_date', 'category']
+
+    ordering_fields = ['published_date', 'category']
+
+    filterset_class = BlogPostFilter
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
